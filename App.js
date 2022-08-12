7a13e0079e81002727339bb384e9b0b5baf51491
jkim8 <juno0218@naver.com>
2022-08-12 14:02:47 -0400

    preload assets and fonts using useAssets and useFonts hooks

--- App.js
+++ App.js
@@ -2,24 +2,15 @@ import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import { Text } from "react-native";
 import * as Font from "expo-font";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./sunssc.png"));
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./sunssc.png")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done!!</Text>;
 }
