2bda903539427bf7467855d9c22fb419bf01a527
jkim8 <juno0218@naver.com>
2022-08-17 12:50:22 -0400

    swiper library

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,24 +1,34 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
+import { Dimensions } from "react-native";
 import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const View = styled.View`
+  flex: 1;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => (
-  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movies;
