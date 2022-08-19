e482ea32cb015c79f50430251ec865ea66979fff
jkim8 <juno0218@naver.com>
2022-08-19 15:41:56 -0400

    serch part one

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import styled from "styled-components/native";
 
-const Search = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Search</Text>
-  </View>
-);
+const Container = styled.ScrollView``;
+
+const SearchBar = styled.TextInput`
+  background-color: white;
+  padding: 10px 15px;
+  border-radius: 15px;
+  width: 90%;
+  margin: 10px auto;
+`;
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const onChangeText = () => {
+    (text: string) => setQuery(text);
+  };
+  return (
+    <Container>
+      <SearchBar
+        placeholderTextColor="grey"
+        placeholder="Search for Movie or TV Show"
+        returnKeyType="search"
+        onChangeText={onChangeText}
+      ></SearchBar>
+    </Container>
+  );
+};
 
 export default Search;
