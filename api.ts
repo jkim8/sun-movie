7f526678d9b2df5226512852d297eaccd3cd9044
jkim8 <juno0218@naver.com>
2022-08-18 22:02:13 -0400

    tv screen part one

--- api.ts
+++ api.ts
@@ -1,11 +1,6 @@
 const API_KEY = "302eb9909c18dbe310619aa2bb5c8b12";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-interface BaseResponse {
-  page: number;
-  total_results: number;
-  total_pages: number;
-}
 export interface Movie {
   adult: boolean;
   backdrop_path: string | null;
@@ -23,22 +18,42 @@ export interface Movie {
   vote_count: number;
 }
 
+interface BaseResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+}
+
 export interface MovieResponse extends BaseResponse {
   results: Movie[];
 }
-const trending = () =>
-  fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
-    res.json()
-  );
 
-const upcoming = () =>
-  fetch(
-    `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
-  ).then((res) => res.json());
+export const moviesApi = {
+  trending: () =>
+    fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  upcoming: () =>
+    fetch(
+      `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+  nowPlaying: () =>
+    fetch(
+      `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+};
 
-const nowPlaying = () =>
-  fetch(
-    `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
-  ).then((res) => res.json());
-
-export const moviesApi = { trending, upcoming, nowPlaying };
+export const tvApi = {
+  trending: () =>
+    fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  airingToday: () =>
+    fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  topRated: () =>
+    fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+};
